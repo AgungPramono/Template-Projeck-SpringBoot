1c7cb7627b1ef963709f8e51fd4a7de99b83cbef
Agung Pramono <agungpermadi13@gmail.com>
2016-01-03 22:30:32 +0700

    main class

---
+++ src/main/java/com/agung/template/TemplateProjekApplication.java
@@ -0,0 +1,17 @@
+package com.agung.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class TemplateProjekApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateProjekApplication.class, args);
+    }
+}
